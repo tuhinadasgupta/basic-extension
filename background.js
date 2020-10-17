055c2000e64b35ed4e65fc23097f8630b3407fbe
Tuhina Dasgupta <tuhinad@gwu.edu>
2020-10-17 11:06:43 -0700

    xmlhttprequest

--- background.js
+++ background.js
@@ -3,3 +3,17 @@ document.getElementById("alt-btn").addEventListener("click", myFunction);
 function myFunction(){
   console.log('HELLO');
 }
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', "http://pachira.eba-dsv6itc5.us-west-2.elasticbeanstalk.com/", true);
+xhr.send("10");
+
+xhr.onreadystatechange = processRequest;
+
+function processRequest(e) {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+      let response = JSON.parse(xhr.responseText);
+      console.log(response)
+  }
+  console.log('Hi')
+}
