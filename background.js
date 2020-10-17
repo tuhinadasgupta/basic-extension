0b8480154b793825c8bdd7c9cdb5e77a8e7e7d0b
Tuhina Dasgupta <tuhinad@gwu.edu>
2020-10-17 15:05:24 -0700

    testing communication

--- background.js
+++ background.js
@@ -4,15 +4,15 @@ function myFunction(){
   console.log('HELLO');
 }
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', "http://pachira.eba-dsv6itc5.us-west-2.elasticbeanstalk.com/", true);
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://pachira.eba-dsv6itc5.us-west-2.elasticbeanstalk.com/", true);
 xhr.send("10");
 
 xhr.onreadystatechange = processRequest;
 
 function processRequest(e) {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-      let response = JSON.parse(xhr.responseText);
+  if (xhr.readyState == 4) {
+      var response = JSON.parse(xhr.responseText);
       console.log(response)
   }
   console.log('Hi')
