c89cdd0ba62030841ef58849db99d614cde9bf33
Tuhina Dasgupta <tuhinad@gwu.edu>
2020-12-20 18:42:53 -0800

    error banner

--- account.js
+++ account.js
@@ -1,12 +1,1 @@
-window.onload = onWindowLoad;
-function onWindowLoad(){
-    var stored = localStorage.getItem('login');
-    if(stored.localeCompare('true')==0){
-        //user is logged in
-        document.getElementById('first').style.display = 'none';
-    }
-    else{
-        //user not logged in 
-        document.getElementById('second').style.display = 'none';
-    }
-}
+// add login call here to web app ??
--- general.js
+++ general.js
@@ -7,3 +7,16 @@ document.getElementById("close-btn").addEventListener("click", closeWindow);
 function closeWindow() {
   window.close('','_parent','');
 }
+
+window.onload = onWindowLoading;
+function onWindowLoading(){
+    var stored = localStorage.getItem('login');
+    if(stored.localeCompare('true')==0){
+        //user is logged in
+        document.getElementById('first').style.display = 'none';
+    }
+    else{
+        //user not logged in 
+        document.getElementById('second').style.display = 'none';
+    }
+}
