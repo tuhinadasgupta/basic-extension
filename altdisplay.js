cb26500a4d55277f3a5fd6f2e3bc6f7d9a911bf1
Tuhina Dasgupta <tuhinad@gwu.edu>
2021-01-15 13:28:33 -0800

    remove yelp stuff

--- altdisplay.js
+++
@@ -1,8 +0,0 @@
-window.onload = onWindowLoading;
-function onWindowLoading(){
-    chrome.storage.local.get('company', function (result) {
-        var write = document.getElementById("write-to");
-        console.log(result.company);
-        write.innerText = result.company;
-    });
-}
